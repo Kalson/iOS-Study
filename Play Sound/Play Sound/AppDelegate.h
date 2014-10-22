0c5ef1aab6b7d1451e308b8c8b4c0daa1c4988b2
KaL <Kalson.Kalu@yahoo.com>
2014-10-21 21:56:48 -0700

    initial setup

---
+++ Play Sound/Play Sound/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Play Sound
+//
+//  Created by KaL on 10/21/14.
+//  Copyright (c) 2014 Kalson Kalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Play Sound/Play Sound/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Play Sound
+//
+//  Created by KaL on 10/21/14.
+//  Copyright (c) 2014 Kalson Kalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
