87dc7ddc884d27945ce1e2a800119043271863f3
KaL <Kalson.Kalu@yahoo.com>
2014-11-09 20:02:37 -0800

    update

--- iAd/iAd/ViewController.m
+++ iAd/iAd/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <iAd/iAd.h>
 
-@interface ViewController ()
+@interface ViewController () <ADBannerViewDelegate>
 
 @end
 
@@ -17,11 +18,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    ADBannerView *bannerView = ADAdTypeMediumRectangle
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma iAd Delegate Methods
+
+// when there is connection
+- (void)bannerViewDidLoadAd:(ADBannerView *)banner
+{
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1];
+    banner.alpha = 1; // if there is a connection, we want to bring the AD back into the view
+    [UIView commitAnimations];
+}
+
+- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
+{
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1];
+    banner.alpha = 0; // if there is no a connection, fade out the banner
+    [UIView commitAnimations];
 }
 
 @end
