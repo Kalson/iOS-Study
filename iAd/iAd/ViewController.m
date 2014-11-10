cfc4d50a35e0f9d646067d5d622693d542c40771
KaL <Kalson.Kalu@yahoo.com>
2014-11-10 05:40:13 -0800

    update

--- iAd/iAd/ViewController.m
+++ iAd/iAd/ViewController.m
@@ -14,17 +14,23 @@
 @end
 
 @implementation ViewController
+{
+    ADBannerView *bannerView;
+    BOOL bannerIsVisible;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    ADBannerView *bannerView = ADAdTypeMediumRectangle
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated
+{
+    bannerView = [[ADBannerView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 50, 320, 50)];
+    bannerView.delegate = self;
+    bannerView.alpha = 0;
+    
 }
 
 #pragma iAd Delegate Methods
@@ -32,18 +38,43 @@
 // when there is connection
 - (void)bannerViewDidLoadAd:(ADBannerView *)banner
 {
-    [UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:1];
-    banner.alpha = 1; // if there is a connection, we want to bring the AD back into the view
-    [UIView commitAnimations];
+    
+    NSLog(@"ad shown");
+    
+    if (!bannerIsVisible) {
+        
+//        // If banner isn't part of view hierarchy, add it
+//        if (bannerView.superview == nil)
+//        {
+//            [self.view addSubview:bannerView];
+//        }
+        
+        [self.view addSubview:bannerView];
+        
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:1];
+        banner.alpha = 1; // if there is a connection or the banner is not visible, we want to bring the AD back into the view
+        [UIView commitAnimations];
+
+        bannerIsVisible = YES;
+    }
+    
 }
 
 - (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
 {
-    [UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:1];
-    banner.alpha = 0; // if there is no a connection, fade out the banner
-    [UIView commitAnimations];
+    NSLog(@"Failed to retrieve ad");
+
+    if (bannerIsVisible) {
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:1];
+        banner.alpha = 0; // if there is no a connection, fade out the banner
+        [UIView commitAnimations];
+        
+        bannerIsVisible = NO;
+    }
+    
+    
 }
 
 @end
