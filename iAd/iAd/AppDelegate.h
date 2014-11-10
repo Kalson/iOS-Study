2eeda1601945c1cc9d45fb1afbbd09e09ae674c2
KaL <Kalson.Kalu@yahoo.com>
2014-11-09 19:28:39 -0800

    intial setup

---
+++ iAd/iAd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iAd
+//
+//  Created by KaL on 11/9/14.
+//  Copyright (c) 2014 Kalson Kalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iAd/iAd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iAd
+//
+//  Created by KaL on 11/9/14.
+//  Copyright (c) 2014 Kalson Kalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
